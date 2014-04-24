187ccafa040371f872e89ed9e8bf3aa1ad0ba177
Mark Strendin <mark.strendin@gmail.com>
2014-04-24 09:43:47 -0600

    Added a proxy for XML and JSON data, so that we can load it from external IPs using AJAX without it complaining about it.

---
+++ LSKYDashboardDataCollector/Proxy/XML.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Template.Master" AutoEventWireup="true" CodeBehind="XML.aspx.cs" Inherits="LSKYDashboardDataCollector.Proxy.XML" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Body" runat="server">
+</asp:Content>
---
+++ LSKYDashboardDataCollector/Proxy/XML.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LSKYDashboardDataCollector.Proxy
+{
+    public partial class XML : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Get the URL to actually load from the querystring
+
+            if (!string.IsNullOrEmpty(Request.QueryString["URL"]))
+            {
+                string UrlToLoad = Request.QueryString["URL"];
+
+                // Attempt to load the requested URL                
+                using (WebClient client = new WebClient())
+                {
+                    byte[] dataReturned = client.DownloadData(UrlToLoad);
+                    Response.Clear();
+                    Response.ContentType = "text/xml";
+                    Response.BinaryWrite(dataReturned);
+                    Response.End();
+                }
+            }
+        }
+    }
+}
